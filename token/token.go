41f9b543a8b45110f88293927129299334964f46
ichisemasashi <ichise.masashi@gmail.com>
2018-06-29 23:44:28 +0900

    add comments.

--- token/token.go
+++ token/token.go
@@ -5,30 +5,32 @@ package token
 type TokenType string
 
 type Token struct {
-	Type TokenType
-	Literal string
+    Type TokenType
+    Literal string
 }
 
 const (
-	ILLEGAL = "ILLEGAL"
-	EOF = "EOF"
+    ILLEGAL = "ILLEGAL"
+    EOF = "EOF"
 
-	IDENT = "IDENT" // add, foobar, x, y, ...
-	INT = "INT" // 1343456
+    // 識別子 + リテラル
+    IDENT = "IDENT" // add, foobar, x, y, ...
+    INT = "INT" // 1343456
 
-	ASSIGN = "="
-	PLUS = "+"
+    // デリミタ
+    ASSIGN = "="
+    PLUS = "+"
 
-	COMMA = ","
-	SEMICOLON = ";"
+    // デリミタ
+    COMMA = ","
+    SEMICOLON = ";"
 
-	LPAREN = "("
-	RPAREN = ")"
-	LBRACE = "{"
-	RBRACE = "}"
+    LPAREN = "("
+    RPAREN = ")"
+    LBRACE = "{"
+    RBRACE = "}"
 
-	FUNCTION = "FUNCTION"
-	LET = "LET"
+    // キーワード
+    FUNCTION = "FUNCTION"
+    LET = "LET"
 )
-
-
