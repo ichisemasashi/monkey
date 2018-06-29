cf05dce31a27a0569328ef9c5b187923bda5f2cb
ichisemasashi <ichise.masashi@gmail.com>
2018-06-29 23:41:29 +0900

    definition of TOKEN.

---
+++ token/token.go
@@ -0,0 +1,34 @@
+// FILE NAME token/token.go
+
+package token
+
+type TokenType string
+
+type Token struct {
+	Type TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF = "EOF"
+
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT = "INT" // 1343456
+
+	ASSIGN = "="
+	PLUS = "+"
+
+	COMMA = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	FUNCTION = "FUNCTION"
+	LET = "LET"
+)
+
+
