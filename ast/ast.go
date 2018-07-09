9c9bbf88fc87cd6e64c95308db2a2d401602d6bb
ichisemasashi <ichise.masashi@gmail.com>
2018-07-09 23:14:35 +0900

    add AST.

---
+++ ast/ast.go
@@ -0,0 +1,31 @@
+// FILE NAME: ast/ast.go
+
+package ast
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+
